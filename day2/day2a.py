61ff8d8ed5ec9e8a6445fac9c4c84742c93a2b6b
JavierGalvez <javi@kbt.localdomain>
2018-12-01 19:09:14 +0100

    2017 desplazado

--- day2/day2a.py
+++
@@ -1,7 +0,0 @@
-f = open('data.txt', 'r')
-numbers = [ list(map(int,line.replace('\n', '').split('\t'))) for line in f ]
-
-checksum = 0;
-for rows in numbers:
-    checksum += max(rows) - min(rows)
-print(checksum)
