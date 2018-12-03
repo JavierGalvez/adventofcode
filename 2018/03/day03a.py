4410e01a7a26e2c8b03fcce21d58c3a01ac69fd2
JavierGalvez <javigalvezobispo@gmail.com>
2018-12-03 14:00:45 +0100

    Day 3 changes

--- 2018/03/day03a.py
+++ 2018/03/day03a.py
@@ -1,17 +1,14 @@
 from collections import defaultdict
-input = [line.replace('@', '').split() for line in open('input.txt')]
 
-for i, claim in enumerate(input):
+overlaps = defaultdict(int)
+for claim in open('input.txt'):
+   claim = claim.replace('@','').split()
    id = int(claim[0][1:])
    loff, toff = map(int, claim[1][:-1].split(','))
    width, height = map(int, claim[2].split('x'))
-   input[i] = [id, loff, toff, width, height]
-
-overlaps = defaultdict(int)
-for claim in input:
-   for i in range(claim[3]):
-      for j in range(claim[4]):
-         overlaps[(claim[1]+i, claim[2]+j)] += 1
+   for i in range(width):
+      for j in range(height):
+         overlaps[(loff+i, toff+j)] += 1
 
 ret = 0
 for k, v in overlaps.items():
--- 2018/03/day03b.py
+++ 2018/03/day03b.py
@@ -1,17 +1,18 @@
 from collections import defaultdict
-input = [line.replace('@', '').split() for line in open('input.txt')]
 
-for i, claim in enumerate(input):
+input = []
+overlaps = defaultdict(int)
+
+for claim in open('input.txt'):
+   claim = claim.replace('@', '').split()
    id = int(claim[0][1:])
    loff, toff = map(int, claim[1][:-1].split(','))
    width, height = map(int, claim[2].split('x'))
-   input[i] = [id, loff, toff, width, height]
+   input.append([id, loff, toff, width, height])
 
-overlaps = defaultdict(int)
-for claim in input:
-   for i in range(claim[3]):
-      for j in range(claim[4]):
-         overlaps[(claim[1]+i, claim[2]+j)] += 1
+   for i in range(width):
+      for j in range(height):
+         overlaps[(loff+i, toff+j)] += 1
 
 for claim in input:
    no_overlap = True
