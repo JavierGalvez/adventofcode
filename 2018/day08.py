45b47da903d5e2ca3daa1b685b1b7ca01e2761c3
JavierGalvez <javigalvezobispo@gmail.com>
2018-12-08 13:33:11 +0100

    Day 8

---
+++ 2018/day08.py
@@ -0,0 +1,22 @@
+data = list(map(int, open('inputs/8.txt').read().split()))
+
+def helper(data):
+	num_branches, num_metadata = data.pop(0), data.pop(0)
+	sum_metadata = 0
+	values = []
+
+	for _ in range(num_branches):
+		sum_branch, data, val = helper(data)
+		sum_metadata += sum_branch
+		values.append(val)
+	
+	sum_metadata += sum(data[:num_metadata])
+
+	if num_branches == 0:
+		return sum_metadata, data[num_metadata:], sum_metadata
+	else:
+		return sum_metadata, data[num_metadata:], sum(values[k-1] for k in data[:num_metadata] if k-1 < num_branches)
+	
+sum_metadatas, _, root_value = helper(data)
+print('Part 1:', sum_metadatas)
+print('Part 2:', root_value)
