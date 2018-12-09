a247c8d49af1cf990a0a34a90d1d884bc46f0300
JavierGalvez <javigalvezobispo@gmail.com>
2018-12-09 12:28:32 +0100

    Day 9

---
+++ 2018/day09.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+from collections import deque
+
+data = open('inputs/9.txt').read().split()
+
+def play(player, n):
+	scores = defaultdict(int)
+	marbles = deque([0])
+	for k in range(1, n+1):
+		if k % 23 == 0:
+			marbles.rotate(-7)
+			scores[k%players] += marbles.pop() + k
+		else:
+			marbles.rotate(2)
+			marbles.append(k)
+	return max(scores.values())
+
+players, marbles = int(data[0]), int(data[6])
+print('Part 1:', play(players, marbles))
+print('Part 2:', play(players, marbles*100))
